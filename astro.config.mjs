3307320d58c1926aea1f3642bd1e7c21df66f059
suzu-shika <msz.006.ts@gmail.com>
2025-06-03 15:50:44 +0900

    2025-06-03_Basic認証03

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: 'never', // ← URLの末尾スラッシュなしを明示
+  publicDir: './public',
 });
