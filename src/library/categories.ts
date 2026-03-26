441c0d4a430341cb79ed309a983a68993a498498
mimomi-youchien <mimomi_youchien@tkcnet.ne.jp>
2026-03-26 15:53:09 +0900

    Update categories.ts

--- src/library/categories.ts
+++ src/library/categories.ts
@@ -1,7 +1,9 @@
 export const CATEGORY_MAP: Record<string, string> = {
   information: "ご連絡",
+  "mi-shuen-jievent": "未就園児イベント",
+  visit: "見学案内",
   recruitment: "募集案内",
-  mimomiNikki: "みもみ日記",
+  "mimomi-nikki": "みもみ日記",
   firstYear: "年少クラス",
   secondYear: "年中クラス",
   thirdYear: "年長クラス",
